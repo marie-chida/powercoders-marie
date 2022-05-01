01a246bb3230432a925c8e147263b746debb6f3f
marie-chida <marie.chida@powercoders.org>
2022-05-01 20:23:06 +0200

    add arrayExercise

---
+++ week05/arrayExercise.js
@@ -0,0 +1,23 @@
+let array = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+console.log(array[2]);
+
+console.log(array);
+
+array.shift();
+
+console.log(array);
+
+array.sort();
+
+console.log(array);
+
+array.push('Kiwi');
+
+console.log(array);
+
+array.splice(0,1);
+
+console.log(array);
+
+console.log(array.reverse);
